27267eedc6caa1485c84e6b37af56e509f49c2b2
Zwiqler94 <jazwickler@gmail.com>
2015-04-26 13:38:14 -0400

    add color type to node!

--- node.h
+++ node.h
@@ -1,15 +1,18 @@
 #ifndef NODE_H_
 #define NODE_H_
 
+enum type{RED, BLACK};
+
 struct node
 {
     node *right;
     node *left;
     node *parent;
+    type color; 
     int height;
     double data;
-    node():right(nullptr),left(nullptr),parent(nullptr),height(1),data(0){}
-    node(double dta):right(nullptr),left(nullptr),parent(nullptr),height(1),data(dta){}
+    node():right(nullptr),left(nullptr),parent(nullptr),height(1),data(0),color(RED){}
+    node(double dta):right(nullptr),left(nullptr),parent(nullptr),height(1),data(dta),color(RED){}
 };
 
 #endif
